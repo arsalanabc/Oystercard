ce345d24c3f815b26bb74662b31250a393b5788a
arsalanabc <arsalanabc@gmail.com>
2018-05-21 14:53:27 +0100

    C3 - unit test for oystercard class: initial balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,18 @@
+require 'Oystercard' 
+
 describe Oystercard do
+
+	describe 'responds to methods' do
+		it 'responds to the method balance' do
+			expect(subject).to respond_to(:balance)
+		end
+	end
+
+	describe '#balance' do 
+		it 'contains default balance of 0' do
+			expect(subject.balance).to eq 0
+		end
+	end
+
+
 end

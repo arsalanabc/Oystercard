72ba31bc7c240e8065de7e9a3a16ba6e6d26afc4
arsalanabc <arsalanabc@gmail.com>
2018-05-21 15:43:06 +0100

    maximum balance

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,11 +1,23 @@
 class Oystercard 
 
 	DEFAULT_BALANCE = 0
+	MAXIMUM_BALANCE = 90
 
 	attr_reader :balance
 
 	def initialize
 		@balance = DEFAULT_BALANCE
+	end
+
+	def topup(amount)
+		fail "Cannot top up: maximum balance of £#{MAXIMUM_BALANCE} reached" if maximum?(amount)
+		@balance += amount
+	end
+
+	private
+
+	def maximum?(amount)
+		@balance + amount > MAXIMUM_BALANCE
 	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,11 +6,27 @@ describe Oystercard do
 		it 'responds to the method balance' do
 			expect(subject).to respond_to(:balance)
 		end
+		it 'responds to the topup balance' do
+			expect(subject).to respond_to(:topup).with(1).argument
+		end
 	end
 
 	describe '#balance' do 
 		it 'contains default balance of 0' do
 			expect(subject.balance).to eq 0
+		end
+	end
+
+	describe '#topup' do 
+		it 'tops up balance' do
+			value = 22
+			expect{ subject.topup(value) }.to change{ subject.balance }.by(value)
+		end
+
+		it 'raises error when maximum balance is reached' do
+			b = Oystercard.new
+			b.topup(90)
+			expect { b.topup(1) }.to raise_error "Cannot top up: maximum balance of £#{ Oystercard::MAXIMUM_BALANCE } reached"
 		end
 	end
 
